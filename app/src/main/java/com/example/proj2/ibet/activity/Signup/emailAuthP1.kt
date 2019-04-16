6ec15777b04b86ef359fb91b96f67778e1fa4fb7
jeanne_hu <hu.jiaqi@wustl.edu>
2019-04-16 16:35:55 -0500

    signup page1

--- app/src/main/java/com/example/proj2/ibet/activity/Signup/emailAuthP1.kt
+++ app/src/main/java/com/example/proj2/ibet/activity/Signup/emailAuthP1.kt
@@ -1,34 +1,64 @@
 package com.example.proj2.ibet.activity.Signup
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.example.proj2.ibet.R
-import android.text.TextUtils
+import android.text.Editable
+import android.text.TextWatcher
 import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
-import android.util.Patterns
 import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
 import com.wajahatkarim3.easyvalidation.core.view_ktx.validEmail
 import com.wajahatkarim3.easyvalidation.core.view_ktx.validator
-import kotlinx.android.synthetic.main.activity_email_auth.*
+import kotlinx.android.synthetic.main.activity_email_auth_p1.*
+import android.R.attr.password
+import android.R
+import com.ybs.passwordstrengthmeter.PasswordStrength
 
 
 class emailAuthP1 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_email_auth)
-        var emailStr = email.text.toString()
+        setContentView(com.example.proj2.ibet.R.layout.activity_email_auth_p1)
+        var emailtxt = findViewById<EditText>(com.example.proj2.ibet.R.id.email)
         //var passwordStr = password.text.toString()
 
-        var passwordtxt = findViewById<EditText>(R.id.password)
-        emailStr.validEmail() {
-            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
-        }
-        passwordtxt.validator().minLength(8).addErrorCallback {
-            passwordtxt.error = it
-        }.check()
+        var passwordtxt = findViewById<EditText>(com.example.proj2.ibet.R.id.password)
+
+        emailtxt.addTextChangedListener (object : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                emailtxt.validator().validEmail().addErrorCallback {
+                    emailtxt.error = it
+                }.check()
+            }
+
+        })
+
+        passwordtxt.addTextChangedListener (object : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                passwordtxt.validator().minLength(8).addErrorCallback {
+                    passwordtxt.error = it
+                }.check()
+                updatePasswordStrenth(s.toString())
+            }
+
+        })
 
         checkBox1.setOnCheckedChangeListener() { buttonView, isChecked ->
             if (!isChecked) {
@@ -48,8 +78,38 @@ class emailAuthP1 : AppCompatActivity() {
             }
         }
 
-        continue1
+        continue1.setOnClickListener {
+            startActivity(Intent(applicationContext, emailAuthP2::class.java))
+        }
 
+    }
+
+    private fun updatePasswordStrenth(password : String) {
+        var progressBar = findViewById<ProgressBar>(com.example.proj2.ibet.R.id.progressBar)
+        var strengthView = findViewById<TextView>(com.example.proj2.ibet.R.id.password_strength)
+        if (TextView.VISIBLE != strengthView.visibility) {
+            return
+        }
+        if (password.isEmpty()) {
+            strengthView.setText("")
+            progressBar.setProgress(0)
+            return
+
+        }
+        val str = PasswordStrength.calculateStrength(password)
+        strengthView.setText(str.getText(this))
+        strengthView.setTextColor(str.getColor())
+
+        progressBar.progressDrawable.setColorFilter(str.getColor(), android.graphics.PorterDuff.Mode.SRC_IN)
+        if (str.getText(this).equals("Weak")) {
+            progressBar.progress = 25
+        } else if (str.getText(this).equals("Medium")) {
+            progressBar.progress = 50
+        } else if (str.getText(this).equals("Strong")) {
+            progressBar.progress = 75
+        } else {
+            progressBar.progress = 100
+        }
     }
 
 
---
+++ app/src/main/java/com/example/proj2/ibet/activity/Signup/emailAuthP2.kt
@@ -0,0 +1,12 @@
+package com.example.proj2.ibet.activity.Signup
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.proj2.ibet.R
+
+class emailAuthP2: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email_auth_p2)
+    }
+}
