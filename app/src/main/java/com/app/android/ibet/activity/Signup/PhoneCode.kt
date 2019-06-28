371a69cbf5cdb44dac5d5669188966318c4a2e5e
Jennie Hu <jenniehu@Jennies-MacBook-Pro.local>
2019-06-28 13:46:02 -0700

    add phone code error

--- app/src/main/java/com/app/android/ibet/activity/Signup/PhoneCode.kt
+++ app/src/main/java/com/app/android/ibet/activity/Signup/PhoneCode.kt
@@ -1,6 +1,7 @@
 package com.app.android.ibet.activity.Signup
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.text.Editable
@@ -28,8 +29,15 @@ class PhoneCode : AppCompatActivity() {
                 veriCodeJson.put("code",code1.text.toString() + code2.text.toString() + code3.text.toString() + code4.text.toString())
                 //http://10.0.2.2:8000/users/api/generateactivationcode/
                 val info = Signup().post(veriCodeJson.toString(), BuildConfig.VERI_CODE )
-                println(info)
-                startActivity(Intent(applicationContext, Verified::class.java))
+                //println(info)
+                val status = info.split(":")[1]
+                println (status)
+                if (status.substring(1,status.length - 2) == "Success") {
+                    startActivity(Intent(applicationContext, Verified::class.java))
+                } else {
+                    code_error.text = "Sorry, that's not the code we're looking for. Please check and try again."
+                    code_error.setTextColor(Color.RED)
+                }
             }
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
