a2e7dfff03a5e69d47faeb1fd85c1594ecd99272
Jennie Hu <jiaqi@claymoreusa.com>
2019-09-20 10:35:27 -0700

    change urls pos

--- app/src/main/java/com/app/android/ibet/api/URLs.kt
+++ app/src/main/java/com/app/android/ibet/api/URLs.kt
@@ -45,6 +45,13 @@ public final class URLs {
 
         val LOG = BuildConfig.BASE_URL + "/system/api/logstreamtos3/"
 
+        val GAME_URL = BuildConfig.BASE_URL + "/games/api/games/?type="
+        val GAME_URL_CATEGORY = BuildConfig.BASE_URL + "&category="
+        val GAME_FILTER= BuildConfig.BASE_URL + "/games/api/filter/"
+
+
+
+
     }
 
 }
